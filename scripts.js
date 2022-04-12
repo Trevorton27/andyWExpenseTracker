ba951ca6ff7704374527321a22f8429c2ca40431
Andy Wang <andywang@Andys-MacBook-Pro.local>
2022-04-11 22:17:37 -0700

    final final commit

--- scripts.js
+++ scripts.js
@@ -7,13 +7,12 @@ const addExpenseButton = document.getElementById("addExpenseButton")
 addExpenseButton.addEventListener('click', (e) => {
     e.stopPropagation()
     const expenseItem = {
-        place: expensePlace.value,
-        name: expenseName.value,
-        amount: expenseAmount.value,
         date: expenseDate.value,
+        name: expenseName.value,
+        place: expensePlace.value,
+        amount: expenseAmount.value,
         id: Date.now()
     }
-    console.log('expenseItem ', expenseItem)
     submitExpense(expenseItem)
 })
     
@@ -31,25 +30,22 @@ function createExpense(expense) {
     row.setAttribute('id', expense.id)
     const keys = Object.keys(expense)
     const cell = []
-    console.log(expense[keys[j]])
-    for(var j = 0; j < keys.length; j++){
-        cell[j]= row.insertCell[j]
+
+    for(var j = 0; j < keys.length-1; j++){
+        cell[j]= row.insertCell(j)
         cell[j].innerText = expense[keys[j]]
     }
-    
-    for(i = 0; i < inputs-1; i++) {
-        cell[i] = row.insertCell(i)
-        cell[i].innerText = expenseInputs[i]   
-    }
-    createDeleteButton(row)
 
+    createDeleteButton(row, expense)
+    
     const placeHolder = document.getElementById('if-empty')
     table.appendChild(placeHolder)
 }
-function createDeleteButton(row){
+
+function createDeleteButton(row, expense){
     const deleteButton = document.createElement('button')
     deleteButton.className = 'deleteButton'
-    deleteButton.setAttribute('id', expenseInputs[4].id)
+    deleteButton.id = expense.id
     deleteButton.textContent = 'X'
     row.appendChild(deleteButton)
     
